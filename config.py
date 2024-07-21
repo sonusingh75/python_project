600713c9de21c3b7d026a41c194d46cf6faf74e0
sonusingh75 <sonusingh03012@gmail.com>
2024-07-21 23:53:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-import os
+import os  # Import the os module, which provides a way to interact with the operating system
 
 class Config:
+    # Set the URI for the SQLAlchemy database
+    # It tries to get the 'DATABASE_URL' environment variable, and if not found, it defaults to using a local SQLite database named 'employees.db'
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///employees.db')
+    
+    # Disable SQLAlchemy event system to save resources and improve performance
     SQLALCHEMY_TRACK_MODIFICATIONS = False
